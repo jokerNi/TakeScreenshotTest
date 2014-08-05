0e5a7eb3e849edcbfc186953a3deda38331e699a
colin <hitwhylz@163.com>
2014-08-05 16:11:39 +0800

    TakeScreenshot
    A screenshot of demo for detecting a user operation

--- TakeScreenshotTest/AppDelegate.m
+++ TakeScreenshotTest/AppDelegate.m
@@ -10,10 +10,111 @@
 
 @implementation AppDelegate
 
+/**
+ *  截取当前屏幕
+ *
+ *  @return NSData *
+ */
+- (NSData *)dataWithScreenshotInPNGFormat
+{
+    CGSize imageSize = CGSizeZero;
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (UIInterfaceOrientationIsPortrait(orientation))
+        imageSize = [UIScreen mainScreen].bounds.size;
+    else
+        imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
+    
+    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    for (UIWindow *window in [[UIApplication sharedApplication] windows])
+    {
+        CGContextSaveGState(context);
+        CGContextTranslateCTM(context, window.center.x, window.center.y);
+        CGContextConcatCTM(context, window.transform);
+        CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
+        if (orientation == UIInterfaceOrientationLandscapeLeft)
+        {
+            CGContextRotateCTM(context, M_PI_2);
+            CGContextTranslateCTM(context, 0, -imageSize.width);
+        }
+        else if (orientation == UIInterfaceOrientationLandscapeRight)
+        {
+            CGContextRotateCTM(context, -M_PI_2);
+            CGContextTranslateCTM(context, -imageSize.height, 0);
+        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
+            CGContextRotateCTM(context, M_PI);
+            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
+        }
+        if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)])
+        {
+            [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
+        }
+        else
+        {
+            [window.layer renderInContext:context];
+        }
+        CGContextRestoreGState(context);
+    }
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return UIImagePNGRepresentation(image);
+}
+
+/**
+ *  返回截取到的图片
+ *
+ *  @return UIImage *
+ */
+- (UIImage *)imageWithScreenshot
+{
+    NSData *imageData = [self dataWithScreenshotInPNGFormat];
+    return [UIImage imageWithData:imageData];
+}
+
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    
+    //注册通知
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(userDidTakeScreenshot:)
+                                                 name:UIApplicationUserDidTakeScreenshotNotification object:nil];
     return YES;
+}
+
+//截屏响应
+- (void)userDidTakeScreenshot:(NSNotification *)notification
+{
+    NSLog(@"检测到截屏");
+    
+    //人为截屏, 模拟用户截屏行为, 获取所截图片
+    UIImage *image_ = [self imageWithScreenshot];
+    
+    //添加显示
+    UIImageView *imgvPhoto = [[UIImageView alloc]initWithImage:image_];
+    imgvPhoto.frame = CGRectMake(self.window.frame.size.width/2, self.window.frame.size.height/2, self.window.frame.size.width/2, self.window.frame.size.height/2);
+    
+    //添加边框
+    CALayer * layer = [imgvPhoto layer];
+    layer.borderColor = [
+                         [UIColor whiteColor] CGColor];
+    layer.borderWidth = 5.0f;
+    //添加四个边阴影
+    imgvPhoto.layer.shadowColor = [UIColor blackColor].CGColor;
+    imgvPhoto.layer.shadowOffset = CGSizeMake(0, 0);
+    imgvPhoto.layer.shadowOpacity = 0.5;
+    imgvPhoto.layer.shadowRadius = 10.0;
+    //添加两个边阴影
+    imgvPhoto.layer.shadowColor = [UIColor blackColor].CGColor;
+    imgvPhoto.layer.shadowOffset = CGSizeMake(4, 4);
+    imgvPhoto.layer.shadowOpacity = 0.5;
+    imgvPhoto.layer.shadowRadius = 2.0;
+
+    [self.window addSubview:imgvPhoto];
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
